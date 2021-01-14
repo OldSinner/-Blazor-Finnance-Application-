9abd6f1d622039ff3252e505ec0b9594cb6ff62b
OldSinner <antylifepatia@gmail.com>
2021-01-14 21:02:46 +0100

    Added ShopList and Modal Component

---
+++ FinnanceApp/Client/Models/EditModal.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace FinnanceApp.Client.Models
+{
+    public class EditModal : ComponentBase
+    {
+        [Parameter]
+        public string Title {get;set;}
+        [Parameter]
+        public string Message{get;set;}
+        
+        public string name{get;set;} 
+        protected bool Confirm {get;set;}
+
+        public void Show()
+        {
+            Confirm=true;
+            StateHasChanged();
+        }
+        [Parameter]
+        public EventCallback<string> ConfirmChanged {get;set;}
+
+        protected async Task OnConfirmChange(string value)
+        {
+            Confirm=false;
+            await ConfirmChanged.InvokeAsync(value);
+        }
+    }
+}
---
+++ FinnanceApp/Client/Models/Modal.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace FinnanceApp.Client.Models
+{
+    public class Modal : ComponentBase
+    {
+        [Parameter]
+        public string Title {get;set;}
+        [Parameter]
+        public string Message{get;set;}
+        protected bool Confirm {get;set;}
+
+        public void Show()
+        {
+            Confirm=true;
+            StateHasChanged();
+        }
+        [Parameter]
+        public EventCallback<bool> ConfirmChanged {get;set;}
+
+        protected async Task OnConfirmChange(bool value)
+        {
+            Confirm=false;
+            await ConfirmChanged.InvokeAsync(value);
+        }
+    }
+}
